a1d0d2ee0a4121a62f41df366fa3f9e9c7a3737e
ian1017 <mdawidamengo@gmail.com>
2020-05-21 20:19:53 +0300

    Test if user object is saved to the contact list

--- user_credentials_test.py
+++ user_credentials_test.py
@@ -18,12 +18,22 @@ class TestUser(unittest.TestCase):
 
     def test__init__(self):
         '''
-        Test if the object is initialized properly.
+        Test to test if the object is initialized properly.
         '''
         self.assertEqual(self.new_user.first_name, "Pop")
         self.assertEqual(self.new_user.last_name, "Smoke")
         self.assertEqual(self.new_user.password, "big092")
 
+
+    def test_save_user(self):
+        '''
+        Test case to test if the user object is saved to the users list.
+        '''
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list, 1))
+
+
+        
 
 if __name__ == '__main__':
     unittest.main()
